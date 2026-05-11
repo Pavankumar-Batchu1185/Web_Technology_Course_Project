4283f3f9b84344f4ef85f5a19ea1c06a914545e5
Pavankumar-Batchu1185 <v.n.s.pavankumar.batchu@gmail.com>
2026-05-11 10:48:02 +0530

    Configured admin panel

--- build.sh
+++ build.sh
@@ -4,4 +4,12 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@gmail.com', '12345678')
+    print('Superuser created!')
+else:
+    print('Superuser already exists.')
+"
 python manage.py shell -c "from qa.models import Category; Category.objects.get_or_create(name='Programming'); Category.objects.get_or_create(name='Mathematics'); Category.objects.get_or_create(name='Science'); Category.objects.get_or_create(name='Web Development'); Category.objects.get_or_create(name='Data Science'); print('Categories created!')"
